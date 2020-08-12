e1c9ee4c23357f191c62cee9dcab3059c807803e
林佳钰 <linjiayu@meituan.com>
2020-08-12 16:15:15 +0800

    Updates

---
+++ precache-manifest.4e39570d55f098e975a29e0725fb01c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27b793199720ce9f1ad9e3ad173b1a2a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f5d91666be646806657d",
+    "url": "./static/css/main.dc0a14bb.chunk.css"
+  },
+  {
+    "revision": "67a6b61e3c815ed25f6c",
+    "url": "./static/js/2.180d86fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.180d86fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5d91666be646806657d",
+    "url": "./static/js/main.f3c3937e.chunk.js"
+  },
+  {
+    "revision": "ca8051aa2fe5b3ef84c3",
+    "url": "./static/js/runtime-main.673723db.js"
+  }
+]);
