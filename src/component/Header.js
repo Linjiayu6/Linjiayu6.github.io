11742e8ff50ab34d4b1702c34e833fa738b99aca
林佳钰 <linjiayu@bytedance.com>
2020-09-21 21:46:14 +0800

    fix

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,10 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Lottie from 'lottie-react-web'
+import axios from 'axios'
 
-const Header = () => (
-  <header>
-    <h1>Lin. JY</h1>
-    <p>🤠 An engineer and explorer. Let's improve everything.</p>
-  </header>
-)
+/**
+ * lottieJSON
+ * assets: 图片
+ * fr: 帧率
+ * h / w: 高 宽
+ * layers: 图层
+ * nm: name
+ */
+const Header = () => {
+  const [lottieJSON, setLottie] = useState(null)
+
+  useEffect(() => {
+    const fetch = async () => {
+      const body = await axios.get('https://assets7.lottiefiles.com/private_files/lf30_AGoC3n.json')
+      if (body && body.data) {
+        const data = body.data
+        setLottie(data)
+      }
+    }
+
+    fetch()
+  }, [])
+
+  return (
+    <header>
+      <h1>Lin. JY</h1>
+      <p>🤠 An engineer and explorer. Let's improve everything.</p>
+      {
+        lottieJSON && <div className="lotte-container">
+          <Lottie 
+            title="lottie"
+            options={{ animationData: lottieJSON }}
+          />
+        </div>
+      }
+    </header>
+  )
+}
 
 export default Header
