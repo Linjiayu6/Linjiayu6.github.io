919f3ea94095a9ea021a082477c4b26045befac6
林佳钰 <linjiayu@bytedance.com>
2020-09-21 16:25:12 +0800

    fix

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,12 +1,10 @@
 import React from 'react'
 
-function Header () {
-  return (
-    <header>
-      <h1>Lin. JY</h1>
-      <p>🤠 An engineer and explorer. Let's improve everything.</p>
-    </header>
-  )
-}
+const Header = () => (
+  <header>
+    <h1>Lin. JY</h1>
+    <p>🤠 An engineer and explorer. Let's improve everything.</p>
+  </header>
+)
 
 export default Header
